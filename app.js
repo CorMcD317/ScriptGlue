6835f5773984733fe552f2639c307e270d3983b9
mcdoucor000 <mcdoucor000@warrern.k12.in.us>
2026-02-04 08:55:33 -0500

    Commit but behind

--- app.js
+++ app.js
@@ -15,8 +15,23 @@ nameSpawn.innerText = name;
 let luckyInput = prompt("pick a lucky number (0-100)");
 let luckyNumber = parseInt(luckyInput);
 
+function luckyNumFix() {
+    let luckyOutput = "";
+    if (isNaN(luckyNumber)) {
+        return "You don't have a lucky number";
+    } else {
+        return luckyNumber.toString();
+    }
+}
+
+let luckyOutput = luckyNumFix();
+
 const luckyNumberSpan = document.getElementById("luckyNumber");
-luckyNumberSpan.innerText = luckyNumber.toString();
+luckyNumberSpan.innerText = luckyOutput;
+
+const facts = [name, luckyOutput];
+
+const statusMessageSpan = document.getElementById("statusMessage");
 
 
 console.log(`Your name is ${name}`);
